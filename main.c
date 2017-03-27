3c77e797c46d41ee64af5e70cd0982394285334b
Maciej Buczyński <kadlubken5@gmail.com>
2017-03-27 16:20:03 +0200

    1st try on the simulation, added some control

---
+++ main.c
@@ -0,0 +1,61 @@
+#include "Ising.h"
+#include "parseGraph.h"
+#include "Graphs.h"
+
+#define N_EDGES 100
+#define N_NODES 5
+
+int main(void)
+{
+    float T = 0.05f;
+    float kb = 1.0f;
+
+    //FILE* f = fopen("wynik10000.txt", "r");
+    //FILE* wynik = fopen("Plik_wynikowy", "w");
+    GRAPH* g = createGraph(g, N_EDGES, N_NODES);
+    connectGraph(g);
+    fillRandomSpins(g);
+    //printGraphAlt(g);
+
+    printf("\n");
+    char choice = 'a';
+    for(int i=0;;i++)
+    {
+
+        choice = getch();
+        switch(choice)
+        {
+            case 's':                           //small step
+                applySmallMCStep(g, T, kb);
+                break;
+            case 'b':                           //big step
+                applyBigMCStep(g, T, kb);
+                break;
+            case 'v':
+                for(int i=0; i<200; i++)        //very big step
+                    applyBigMCStep(g, T, kb);
+                break;
+            case 'e':                           //exit
+                //fclose(f);
+                return 0;
+        }
+        printGraphStats(g);
+
+        //Writing to file
+        /*
+        applySmallMCStep(g, 0.22f, 1.0f);
+        int spinUp = getSpinUpNum(g);
+        fprintf(wynik, "%d %d\n", i, spinUp);
+        if(spinUp == 0 || spinUp ==10000)
+            break;
+            */
+
+
+
+    }
+    //fclose(wynik);
+    //fclose(f);
+
+
+    return 0;
+}
